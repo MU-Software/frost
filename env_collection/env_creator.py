decb6485ff1131e549483823fcf35a8973a529fb
MUsoftware <musoftware@daum.net>
2021-06-20 19:07:50 +0900

    Fix env_creator not finding target file properly because of relative path

--- env_collection/env_creator.py
+++ env_collection/env_creator.py
@@ -11,9 +11,11 @@ def get_traceback_msg(err):
                    tb=err.__traceback__))
 
 
-def json_to_envfiles(json_path: str, output_name: str):
+def json_to_envfiles(output_file: pathlib.Path):
     try:
-        input_env_file_content: str = pathlib.Path(json_path).read_text()
+        output_name: str = output_file.stem
+
+        input_env_file_content: str = output_file.read_text()
         input_env: dict[str, typing.Union[str, dict[str, str]]] = json.loads(input_env_file_content)
 
         template_launch_json_file: pathlib.Path = pathlib.Path('template/launch.json')
@@ -96,11 +98,21 @@ def json_to_envfiles(json_path: str, output_name: str):
 if __name__ == '__main__':
     import os
     import sys
-    os.chdir(pathlib.Path(__file__).parents[0])
     if len(sys.argv) > 1:
-        target_file: str = sys.argv[1]
+        target_file = pathlib.Path(sys.argv[1]).absolute()
+        if not target_file.exists():
+            print(target_file.as_posix())
+            # Ignore just now. We'll retry this after changing paths
+            target_file = None
     else:
         print('Need to specify target environment variables collection file(.json)')
         os._exit(1)
 
-    json_to_envfiles(target_file, target_file.replace('.json', ''))
+    os.chdir(pathlib.Path(__file__).parents[0])
+
+    if target_file is None:
+        target_file = pathlib.Path(sys.argv[1]).absolute()
+        if not target_file.exists():
+            raise FileNotFoundError()
+
+    json_to_envfiles(target_file)
