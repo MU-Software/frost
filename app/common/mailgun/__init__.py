2fccc7d43da9f2cf456c2103c35612fd82cd3cb3
MUsoftware <musoftware@daum.net>
2021-07-23 16:07:15 +0900

    Add send_mail function so that we can use just one function while sending mail

---
+++ app/common/mailgun/__init__.py
@@ -0,0 +1,24 @@
+import flask
+import app.common.mailgun.aws_ses as mailgun_aws
+import app.common.mailgun.gmail as mailgun_gmail
+
+
+def send_mail(fromaddr: str, toaddr: str, subject: str, message: str) -> bool:
+    mail_sent: bool = True
+    if flask.current_app.config.get('MAIL_ENABLE'):
+        try:
+            mail_provider = flask.current_app.config.get('MAIL_PROVIDER', 'AMAZON')
+            if mail_provider == 'AMAZON':
+                mailgun_aws.send_mail(fromaddr=fromaddr, toaddr=toaddr, subject=subject, message=message)
+            elif mail_provider == 'GOOGLE':
+                mailgun_gmail.send_mail(
+                    google_client_id=flask.current_app.config.get('GOOGLE_CLIENT_ID'),
+                    google_client_secret=flask.current_app.config.get('GOOGLE_CLIENT_SECRET'),
+                    google_refresh_token=flask.current_app.config.get('GOOGLE_REFRESH_TOKEN'),
+                    fromaddr=fromaddr, toaddr=toaddr, subject=subject, message=message)
+            else:
+                raise NotImplementedError(f'Mail provider "{mail_provider}" is not supported')
+            mail_sent = True
+        except Exception:
+            mail_sent = False
+    return mail_sent
