bb82f7f47ba2341f82d0cb1119cb3ae2abf23449
MUsoftware <musoftware@daum.net>
2021-08-02 20:24:39 +0900

    Add jwt authentication on admin page
    ...so that only person that has 'admin' string in role can access admin page

--- app/admin/__init__.py
+++ app/admin/__init__.py
@@ -1,11 +1,50 @@
 import flask
 import flask_admin as fadmin
 import flask_admin.contrib.sqla as fadmin_sqla
+import jwt
+
+import app.api.helper_class as api_class
+import app.database.jwt as jwt_module
+
+from app.api.response_case import CommonResponseCase
+from app.api.account.response_case import AccountResponseCase
+
+
+def check_admin_authenticate(in_jwt: str, csrf_token: str) -> tuple[bool, api_class.Response]:
+    '''
+    This returns (True, None) if authenticaion succeed,
+    else (False, <One of AccountResponseCase>) if it fails to authenticate.
+    '''
+    access_token: jwt_module.AccessToken = None
+    try:
+        access_token = jwt_module.AccessToken.from_token(in_jwt, flask.current_app.config.get('SECRET_KEY')+csrf_token)
+    except jwt.exceptions.ExpiredSignatureError:
+        # AccessToken Expired error must be raised when bearer auth is softly required,
+        # so that client can re-request after refreshing AccessToken
+        return (False, AccountResponseCase.access_token_expired.create_response())
+    except Exception:
+        return (False, AccountResponseCase.access_token_invalid.create_response())
+    if not access_token:
+        return (False, AccountResponseCase.user_not_signed_in.create_response())
+
+    if 'admin' in access_token.role:
+        return (True, None)
+    else:
+        return (False, CommonResponseCase.http_forbidden.create_response())
+
+
+def is_accessible_mod(self: fadmin.base.BaseView):
+    csrf_token = flask.request.headers.get('X-Csrf-Token', '')
+    access_token_bearer = flask.request.headers.get('Authorization', '').replace('Bearer ', '')
+    return check_admin_authenticate(access_token_bearer, csrf_token)[0]
 
 
 def init_app(app: flask.Flask, add_model_to_view: bool = True):
     app_name = app.config.get('BACKEND_NAME', 'Backend Core')
     restapi_version = app.config.get('RESTAPI_VERSION')
+
+    # Register flask-admin authenticate
+    fadmin.base.BaseView.is_accessible = is_accessible_mod
     admin = fadmin.Admin(
                 app,
                 name=app_name,
