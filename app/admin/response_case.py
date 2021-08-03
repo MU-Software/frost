6fc9c60a55e723268b40432a2f377a01b1747478
MUsoftware <musoftware@daum.net>
2021-08-03 18:47:44 +0900

    Make admin_token_invalid, admin_token_expired to remove invalid admin token cookie

--- app/admin/response_case.py
+++ app/admin/response_case.py
@@ -1,4 +1,18 @@
+import os
 import app.api.helper_class as api_class
+import app.common.utils as utils
+
+server_name = os.environ.get('SERVER_NAME')
+restapi_version = os.environ.get('RESTAPI_VERSION')
+https_enable = os.environ.get('HTTPS_ENABLE', True)
+
+admin_token_remover_cookie = utils.delete_cookie(
+                                    name='admin_token',
+                                    path=f'/api/{restapi_version}/admin',
+                                    domain=server_name if restapi_version != 'dev' else None,
+                                    samesite='None' if restapi_version == 'dev' else 'strict',
+                                    secure=https_enable)
+delete_admin_token: tuple[str, str] = ('Set-Cookie', admin_token_remover_cookie)
 
 
 class AdminResponseCase(api_class.ResponseCaseCollector):
@@ -6,11 +20,13 @@ class AdminResponseCase(api_class.ResponseCaseCollector):
     admin_token_invalid = api_class.Response(
         description='Admin token cookie is invalid.',
         code=401, success=False,
-        public_sub_code='admin_token.invalid')
+        public_sub_code='admin_token.invalid',
+        header=[delete_admin_token, ])
     admin_token_expired = api_class.Response(
         description='Admin token cookie is expired. Please refresh it.',
         code=401, success=False,
-        public_sub_code='admin_token.expired')
+        public_sub_code='admin_token.expired',
+        header=[delete_admin_token, ])
     admin_forbidden = api_class.Response(
         description='We assumes that you are not a admin, so this action could not be executed.',
         code=401, success=False,
@@ -20,11 +36,13 @@ class AdminResponseCase(api_class.ResponseCaseCollector):
     admin_token_invalid_html = api_class.Response(
         description='Admin token cookie is invalid.',
         code=401, success=False,
-        content_type='text/html', template_path='admin/error/admin_token_invalid.html')
+        content_type='text/html', template_path='admin/error/admin_token_invalid.html',
+        header=[delete_admin_token, ])
     admin_token_expired_html = api_class.Response(
         description='Admin token cookie is expired. Please refresh it.',
         code=401, success=False,
-        content_type='text/html', template_path='admin/error/admin_token_expired.html')
+        content_type='text/html', template_path='admin/error/admin_token_expired.html',
+        header=[delete_admin_token, ])
     admin_forbidden_html = api_class.Response(
         description='We assumes that you are not a admin, so this action could not be executed.',
         code=401, success=False,
