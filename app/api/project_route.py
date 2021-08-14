2a9e0a042e4474cd99934e45bf968383e2651f27
MUsoftware <musoftware@daum.net>
2021-08-14 13:34:39 +0900

    Add comment on project_route and project_table

--- app/api/project_route.py
+++ app/api/project_route.py
@@ -1,2 +1,4 @@
-# Import and add project routes here
+# Import and add project routes here.
+# If you want to make git not to track this file anymore,
+# use `git update-index --skip-worktree app/api/project_route.py`
 project_resource_routes = dict()
--- app/database/project_table.py
+++ app/database/project_table.py
@@ -1,1 +1,3 @@
 # Import project db tables here
+# If you want to make git not to track this file anymore,
+# use `git update-index --skip-worktree app/database/project_table.py`
