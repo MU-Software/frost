d3ed616a67d8a5fbdb58d0744eb7ab621950b1b7
MU-Software <musoftware@daum.net>
2022-02-25 03:15:46 +0900

    Support redis auth on flask-limiter

--- app/plugin/__init__.py
+++ app/plugin/__init__.py
@@ -12,11 +12,17 @@ def init_app(app: flask.Flask):
     runable_app = app
 
     # Rate-limit our API
+    limiter_storage_url = 'redis://'
+    redis_password = app.config.get("REDIS_PASSWORD", None)
+    if redis_password:
+        limiter_storage_url += f':{redis_password}@'
+    limiter_storage_url += f'{app.config.get("REDIS_HOST")}:{app.config.get("REDIS_PORT")}'
+
     limiter = flask_limiter.Limiter(  # noqa: F841
         runable_app,
         key_func=flask_limiter.util.get_remote_address,
         default_limits=['3 per second'],
-        storage_uri=f'redis://{app.config.get("REDIS_HOST")}:{app.config.get("REDIS_PORT")}')
+        storage_uri=limiter_storage_url)
 
     # init_app must return app
     return runable_app
