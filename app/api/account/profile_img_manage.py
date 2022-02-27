ab7aabbdfd41340a8bb0d7c31da732a382681bcc
MU-Software <musoftware@daum.net>
2022-02-28 00:15:32 +0900

    Fix profile image upload route not using internal API correctly

--- app/api/account/profile_img_manage.py
+++ app/api/account/profile_img_manage.py
@@ -1,3 +1,4 @@
+import json
 import flask
 import flask.views
 
@@ -27,43 +28,40 @@ class ProfileImageRoute(flask.views.MethodView, api_class.MethodViewMixin):
             - body_bad_semantics
             - server_error
         '''
-        try:
-            file_upload_enabled: bool = flask.current_app.config.get('FILE_MANAGEMENT_ROUTE_ENABLE', False)
-            if not file_upload_enabled:
-                return CommonResponseCase.http_forbidden.create_response(
-                    message='File upload is not enabled',
-                    data={'reason': 'File upload is not enabled'}, )
+        file_upload_enabled: bool = flask.current_app.config.get('FILE_MANAGEMENT_ROUTE_ENABLE', False)
+        if not file_upload_enabled:
+            return CommonResponseCase.http_forbidden.create_response(
+                message='File upload is not enabled',
+                data={'reason': 'File upload is not enabled'}, )
 
-            target_user = db.session.query(user_module.User)\
-                .filter(user_module.User.locked_at.is_(None))\
-                .filter(user_module.User.deactivated_at.is_(None))\
-                .filter(user_module.User.uuid == access_token.user)\
-                .first()
-            if not target_user:
-                return AccountResponseCase.user_not_found.create_response()
+        target_user = db.session.query(user_module.User)\
+            .filter(user_module.User.locked_at.is_(None))\
+            .filter(user_module.User.deactivated_at.is_(None))\
+            .filter(user_module.User.uuid == access_token.user)\
+            .first()
+        if not target_user:
+            return AccountResponseCase.user_not_found.create_response()
 
-            # We'll handle upload first to make sure whether upload process success.
-            # We can't revert this when error raised if we delete files first.
+        # We'll handle upload first to make sure whether upload process success.
+        # We can't revert this when error raised if we delete files first.
+        # This calls internal REST API
+        up_result: api_class.ResponseType = route_filemgr.FileManagementRoute().post()
+        up_res_body, up_res_code, up_res_header = up_result
+        if up_res_code != 201:  # Upload failed
+            return up_result
+        up_res_body = json.loads(up_res_body.data)  # I hate this
+
+        if target_user.profile_image:
+            target_file: str = target_user.profile_image.replace('/uploads/', '').replace('/', '').strip()
             # This calls internal REST API
-            up_result: api_class.ResponseType = route_filemgr.FileManagementRoute().post()
-            up_res_body, up_res_code, up_res_header = up_result
-            if up_res_code != 201:  # Upload failed
-                return up_result
+            del_result: api_class.ResponseType = route_filemgr.FileManagementRoute().delete(filename=target_file)
+            del_res_body, del_res_code, del_res_header = del_result
+            if del_res_code != 204:  # Deletion didn't done completely.
+                return CommonResponseCase.server_error.create_response()
 
-            if target_user.profile_image:
-                target_file: str = target_user.profile_image.replace('/uploads/', '').replace('/', '').strip()
-                # This calls internal REST API
-                del_result: api_class.ResponseType = route_filemgr.FileManagementRoute().delete(target_file)
-                del_res_body, del_res_code, del_res_header = del_result
-                if del_res_code != 204:  # Deletion didn't done completely.
-                    return CommonResponseCase.server_error.create_response()
-
-            target_user.profile_image = up_res_body['file']['url']
-            db.session.commit()
-            return ResourceResponseCase.resource_modified.create_response()
-        except Exception:
-            db.session.rollback()
-            return CommonResponseCase.server_error.create_response()
+        target_user.profile_image = up_res_body['data']['file']['url']
+        db.session.commit()
+        return ResourceResponseCase.resource_modified.create_response()
 
     @api_class.RequestHeader(auth={api_class.AuthType.Bearer: True, })
     def delete(self, req_header: dict, access_token: jwt_module.AccessToken):
